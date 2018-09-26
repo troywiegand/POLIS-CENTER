a4c1f9a0e5ecaf5a9acc6034bb69eb6aded18972
Troy Wiegand <tmwiegan@butler.edu>
2018-09-26 17:01:06 -0400

    Implement Fake Data
    Signed-off-by: Troy Wiegand <tmwiegan@butler.edu>

--- src/EventPage.js
+++ src/EventPage.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import EventList from './EventList.js';
 
 class EventPage extends Component {
+constructor(){
+  super()
+
+  this.state={
+
+    fakeData: [{id: 1, name: 'Whatever', description: 'yee'},
+              {id: 2, name: 'Adam\'s LoL Match', description: 'He\'s Gold -2 '}]
+  }
+}
+
   render() {
     return (
       <div className="EventPage">
     
           <h1 >Events!!!</h1>
-          <EventList />
+          <EventList fakeData={this.state.fakeData} />
     
       </div>
     );
