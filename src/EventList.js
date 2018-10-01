2ff1ddfc9c9bac600f482a106cc349777b95277e
Troy Wiegand <troy@troywiegand.com>
2018-10-01 16:59:18 -0400

    Make the table display eventDesc formatted  with HTML

--- src/EventList.js
+++ src/EventList.js
@@ -13,7 +13,7 @@ class EventList extends Component {
     }
   }
 
-  componentDidMount(){
+  componentWillMount(){
     fetch("http://in-polis-app27.ads.iu.edu/SpiritAPI/API/Spirit/Events")
     .then(res => res.json())
     .then(
@@ -33,22 +33,25 @@ class EventList extends Component {
     )
   }
 
+  descFormatter = (cell, row) =>{ 
+    console.log(row)
+    let modifiedCell={__html: cell}
+    return <div dangerouslySetInnerHTML={modifiedCell}/>;
+
+  }
+
 
   render() {
     return (
       <div className="EventList">
 
-        <h2 >Put a Table here!</h2>
         <div style={style}>
-        <BootstrapTable data={this.state.items} striped hover condensed  height='120px'>
-      <TableHeaderColumn isKey dataField='eventId'>ID</TableHeaderColumn>
+        <BootstrapTable data={this.state.items} striped hover condensed  keyField='eventID' height='120px' search>
       <TableHeaderColumn dataField='eventTitle'> Name</TableHeaderColumn>
-      <TableHeaderColumn dataField='eventDesc'>Description</TableHeaderColumn>
+      <TableHeaderColumn dataFormat={this.descFormatter} dataField='eventDesc'>Description</TableHeaderColumn>
       </BootstrapTable>
         </div>
-       
-
-
+      
 
       </div>
     );
