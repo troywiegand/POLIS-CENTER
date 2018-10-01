79895659514ae40bcb42e48e81986851c8acc66d
Troy Wiegand <troy@troywiegand.com>
2018-10-01 16:14:35 -0400

    Add API unfiltered data to table

--- src/EventList.js
+++ src/EventList.js
@@ -4,16 +4,46 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 
 class EventList extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      error:null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://in-polis-app27.ads.iu.edu/SpiritAPI/API/Spirit/Events")
+    .then(res => res.json())
+    .then(
+      (result)=>{
+        this.setState({
+          isLoaded: true,
+          items: result
+        })
+      },
+
+      (error)=>{
+        this.setState({
+          isLoaded:true,
+          error
+        })
+      }
+    )
+  }
+
+
   render() {
     return (
       <div className="EventList">
 
         <h2 >Put a Table here!</h2>
         <div style={style}>
-        <BootstrapTable data={this.props.fakeData} striped hover condensed  height='120px'>
-      <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
-      <TableHeaderColumn dataField='name'> Name</TableHeaderColumn>
-      <TableHeaderColumn dataField='description'>Description</TableHeaderColumn>
+        <BootstrapTable data={this.state.items} striped hover condensed  height='120px'>
+      <TableHeaderColumn isKey dataField='eventId'>ID</TableHeaderColumn>
+      <TableHeaderColumn dataField='eventTitle'> Name</TableHeaderColumn>
+      <TableHeaderColumn dataField='eventDesc'>Description</TableHeaderColumn>
       </BootstrapTable>
         </div>
        
