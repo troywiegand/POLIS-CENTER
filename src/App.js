e2468a7dc6dae558b94403729819b154a923d167
Troy Wiegand <troy@troywiegand.com>
2018-10-15 16:42:57 -0400

    Get all three API JSON's not just Events

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import SpecificEventPage from './SpecificEventPage'
-//import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 
 import EventList from './EventList'
@@ -13,31 +12,70 @@ class App extends Component {
     super()
     this.state = {
       error: null,
-      isLoaded: false,
-      items: []
+      items: [],
+      presenters: [],
+      schedule: []
     }
     this.loadAPI()
   }
   
   
 loadAPI = ()=> {
+  //This gets the event info
   fetch("http://in-polis-app27.ads.iu.edu/SpiritAPI/API/Spirit/Events")
     .then(res => res.json())
     .then(
       (result) => {
         this.setState({
-          isLoaded: true,
+          isEventsLoaded: true,
           items: result
         })
       },
 
       (error) => {
         this.setState({
-          isLoaded: true,
+          isEventsLoaded: true,
           error
         })
       }
     )
+//This gets the presenters info 
+    fetch("http://in-polis-app27.ads.iu.edu/SpiritAPI/API/Spirit/Presenters")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isPresentersLoaded: true,
+          presenters: result
+        })
+      },
+
+      (error) => {
+        this.setState({
+          isPresentersLoaded: true,
+          error
+        })
+      }
+    )
+//This gets the info about the schedule
+    fetch("http://in-polis-app27.ads.iu.edu/SpiritAPI/API/Spirit/Schedule")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isScheduleLoaded: true,
+          schedule: result
+        })
+      },
+
+      (error) => {
+        this.setState({
+          isScheduleLoaded: true,
+          error
+        })
+      }
+    )
+
 }
 
 
@@ -50,12 +88,15 @@ loadAPI = ()=> {
         </header>
 
         <Switch>
+         {/*  This is the route for specfic pages */}
           <Route path="/Event/:eventId/:eventTitle"
             render={(navProps) => {
               return (
-                <SpecificEventPage {...navProps} items={this.state.items} loadAPI={this.loadAPI}/>
+                <SpecificEventPage {...navProps} items={this.state.items} 
+                  presenters={this.state.presenters} loadAPI={this.loadAPI}/>
               )
             }} />
+            {/*  This is the route all the events */}
           <Route path="/Event"
             render={() => { return (<EventList items={this.state.items} />) }} />
           <Route render={() => {
