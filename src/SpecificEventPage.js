5a2ce7fd1efa9818149db54296a76f70d486e397
adam <apk@pluvi.us>
2018-10-29 16:11:38 -0400

    im sorry im really salty right now

--- src/SpecificEventPage.js
+++ src/SpecificEventPage.js
@@ -7,22 +7,25 @@ constructor(props){
  let eventNumb= this.props.location.pathname.slice(7,8)
  let eventIndex= parseInt(eventNumb, 10)-2
  this.state={eventIndex,Header: "Header",PresenterList: ""}
- this.props.loadAPI()
 }
 
 componentWillReceiveProps(){
   //This gets the title of the event from this.props.items
   let eventNumb= this.props.location.pathname.slice(7,8)
-  if(this.props.items[this.state.eventIndex]!==undefined)
-    this.setState({Header: this.props.items[this.state.eventIndex].eventTitle})
+  if(this.props.bothArray[this.state.eventIndex]!==undefined)
+    this.setState({Header: this.props.bothArray[this.state.eventIndex].eventTitle,
+		   PresenterList: this.props.bothArray[this.state.eventIndex].PresenterList,
+		   StartDate: this.props.bothArray[this.state.eventIndex].startDate,
+		   EndDate: this.props.bothArray[this.state.eventIndex].endDate,
+		   RSVP: this.props.bothArray[this.state.eventIndex].eventRsvp})
 
     //This part isn't really working
-  if(this.props.presenters!==undefined)
-    this.props.presenters.map((presenter)=>{
-      if(presenter.eventId===eventNumb)
-        this.setState({PresenterList: this.state.PresenterList+presenter.lastName})
-        return null
-    })
+  // if(this.props.presenters!==undefined)
+  //   this.props.presenters.map((presenter)=>{
+  //     if(presenter.eventId===eventNumb)
+  //       this.setState({PresenterList: this.state.PresenterList+presenter.lastName})
+  //       return null
+  //   })
 }
 
 
@@ -38,6 +41,8 @@ componentWillReceiveProps(){
     <h3>
 Presenters Include:
 <div > {this.state.PresenterList}</div>
+<div> Start Date: {this.state.StartDate} </div>
+RSVP: {this.state.RSVP}
     </h3>
       </div>
     );
